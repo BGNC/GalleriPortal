f537ec7eb80fc5b0af025a286c466bac4e911442
apple <apple@apples-MacBook-Air.local>
2024-10-23 20:21:30 +0300

    Initial commit

---
+++ src/main/java/com/bgnc/galleriportal/GalleriPortalApplication.java
@@ -0,0 +1,13 @@
+package com.bgnc.galleriportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleriPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleriPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bgnc/galleriportal/GalleriPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgnc.galleriportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleriPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
