7fb9c1e528277bed5be45b43315e0aa2e9ae16fa
BGNC <bugra34055@icloud.com>
2024-12-03 23:44:33 +0300

    yapıkredi currency rate fetching is started.

--- src/main/java/com/bgnc/galleriportal/service/ICurrencyService.java
+++ src/main/java/com/bgnc/galleriportal/service/ICurrencyService.java
@@ -4,4 +4,5 @@ import com.bgnc.galleriportal.dto.CurrencyRateResponse;
 
 public interface ICurrencyService {
     CurrencyRateResponse getCurrencyRate(String startDate,String endDate);
+    CurrencyRateResponse getCurrencyRateFromBank(String startDate,String endDate);
 }
