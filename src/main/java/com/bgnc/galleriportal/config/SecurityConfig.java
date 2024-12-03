97aa225b9b2999e39b797709ea03ca92eec3b37d
BGNC <bugra34055@icloud.com>
2024-12-03 23:41:43 +0300

    new endpoint is added and permission is chaned.

--- src/main/java/com/bgnc/galleriportal/config/SecurityConfig.java
+++ src/main/java/com/bgnc/galleriportal/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(REGISTER, AUTHENTICATE, REFRESH_TOKEN).permitAll()
