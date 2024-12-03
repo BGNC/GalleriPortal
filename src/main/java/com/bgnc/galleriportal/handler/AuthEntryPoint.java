8d5f69824fffd2fdd634b19de60283aac527950d
BGNC <bugra34055@icloud.com>
2024-12-03 23:39:19 +0300

    authentication entry point is added as a component.

--- src/main/java/com/bgnc/galleriportal/handler/AuthEntryPoint.java
+++ src/main/java/com/bgnc/galleriportal/handler/AuthEntryPoint.java
@@ -10,7 +10,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
+
+
 
 @Component
 public class AuthEntryPoint implements AuthenticationEntryPoint{
